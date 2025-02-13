1d31b3701a6348e20b670d4cd586c844dba4d828
navneetshukl <navneetshukla824@gmail.com>
2025-02-13 21:54:43 +0530

    series of type commands

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -11,8 +11,15 @@ import (
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
 var _ = fmt.Fprint
 
+func init() {
+	log.SetPrefix("trace: ")
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+
+}
+
 func main() {
 
+	TYPE := []string{"echo", "exit", "type"}
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
 		str, err := bufio.NewReader(os.Stdin).ReadString('\n')
@@ -31,6 +38,21 @@ func main() {
 		case "echo":
 			resp = strings.Join(commands[1:], " ")
 			resp = fmt.Sprintf("%s\n", resp)
+
+		case "type":
+			cmdStr := strings.Join(commands[1:], " ")
+			isFound := false
+			for _, v := range TYPE {
+				if v == cmdStr {
+					isFound = true
+					break
+				}
+			}
+			if isFound {
+				resp = fmt.Sprintf("%s %s \n", cmdStr, "is a shell builtin")
+			} else {
+				resp = fmt.Sprintf("%s: %s \n", cmdStr, "not found")
+			}
 
 		default:
 			resp = strings.Join(commands, " ")
