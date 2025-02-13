3bf0de58de0027914f8a4ca67c9489b35cbfeeb5
navneetshukl <navneetshukla824@gmail.com>
2025-02-13 21:20:53 +0530

    writting invalid command to terminal

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -3,16 +3,23 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
+	"strings"
 )
 
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
 var _ = fmt.Fprint
 
 func main() {
-	// Uncomment this block to pass the first stage
 	 fmt.Fprint(os.Stdout, "$ ")
 
-	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+	str,err:=bufio.NewReader(os.Stdin).ReadString('\n')
+	if err!=nil{
+		log.Println("error in reading from the os.Stdin ",err )
+		return
+	}
+	str=strings.Trim(str," \r\n\t")
+	resp:=fmt.Sprintf("%s: %s",str,"command not found")
+	fmt.Fprint(os.Stdout,resp)
 }
