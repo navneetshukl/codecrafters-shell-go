e994c7a7212d7c75b8dfb3cefc2bc73044292b1e
navneetshukl <navneetshukla824@gmail.com>
2025-02-15 15:17:44 +0530

    refactor

--- cmd/myshell/cmd.go
+++ cmd/myshell/cmd.go
@@ -23,7 +23,7 @@ func execCommand(commands []string, pathEnv string) {
 
 		_, exits := allCommands[cmdStr]
 		if exits {
-			resp = fmt.Sprintf("%s %s \n", cmdStr, "is a shell builtin")
+			resp = fmt.Sprintf("%s %s\n", cmdStr, "is a shell builtin")
 			fmt.Fprint(os.Stdout, resp)
 		} else {
 			paths := strings.Split(pathEnv, ":")
@@ -31,13 +31,13 @@ func execCommand(commands []string, pathEnv string) {
 				str := path + "/" + cmdStr
 				_, err := os.Stat(str)
 				if err == nil {
-					resp = fmt.Sprintf("%s is %s \n", cmdStr, str)
+					resp = fmt.Sprintf("%s is %s\n", cmdStr, str)
 					fmt.Fprint(os.Stdout, resp)
 					return
 				}
 			}
 		}
-		resp = fmt.Sprintf("%s: %s \n", cmdStr, "not found")
+		resp = fmt.Sprintf("%s: %s\n", cmdStr, "not found")
 		fmt.Fprint(os.Stdout, resp)
 
 	default:
