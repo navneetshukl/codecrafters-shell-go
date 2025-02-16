340f6d7664f33ef845b16e10631bad3d99f35190
navneetshukl <navneetshukla824@gmail.com>
2025-02-16 11:17:36 +0530

    single quote

--- cmd/myshell/cmd.go
+++ cmd/myshell/cmd.go
@@ -5,38 +5,17 @@ import (
 	"log"
 	"os"
 	"os/exec"
-	"strings"
 )
 
 var allCommands = map[string]int{"echo": 0, "exit": 1, "type": 2, "pwd": 3}
 
 func execCommand(commands []string, pathEnv, homeEnv string) {
-	var resp string
 	switch commands[0] {
 	case "exit":
 		os.Exit(0)
 	case "echo":
-		resp = strings.Join(commands[1:], " ")
-		resp = fmt.Sprintf("%s\n", resp)
 
-		ch1 := resp[0]
-		if string(ch1) == "'" {
-			resp = resp[1:]
-		}
-		if string(resp[len(resp)-2]) == "'" {
-			resp1 := ""
-			for idx, val := range resp {
-				//fmt.Println("index and character is ", idx, string(val))
-				if idx == len(resp)-2 && string(val) == "'" {
-					continue
-				} else {
-					resp1 += string(val)
-				}
-			}
-			resp = resp1
-		}
-		//fmt.Println("Length is ", len(resp))
-		fmt.Fprint(os.Stdout, resp)
+		echoCmd(commands)
 
 	case "type":
 
@@ -50,7 +29,9 @@ func execCommand(commands []string, pathEnv, homeEnv string) {
 		}
 		fmt.Fprintf(os.Stdout, "%s\n", dir)
 	case "cd":
+
 		cdCmd(commands, homeEnv)
+
 	default:
 		command := exec.Command(commands[0], commands[1:]...)
 		command.Stderr = os.Stderr
---
+++ cmd/myshell/echoCmd.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func echoCmd(commands []string) {
+	resp := strings.Join(commands[1:], " ")
+	resp = fmt.Sprintf("%s\n", resp)
+
+	ch1 := resp[0]
+	if string(ch1) == "'" {
+		resp = resp[1:]
+	}
+	if string(resp[len(resp)-2]) == "'" {
+		resp1 := ""
+		for idx, val := range resp {
+			if idx == len(resp)-2 && string(val) == "'" {
+				continue
+			} else {
+				resp1 += string(val)
+			}
+		}
+		resp = resp1
+	}
+	fmt.Fprint(os.Stdout, resp)
+}
