3d0a15051730edd7a20535966105f48495b33a8a
navneetshukl <navneetshukla824@gmail.com>
2025-02-16 10:32:29 +0530

    refactor

--- cmd/myshell/cmd.go
+++ cmd/myshell/cmd.go
@@ -12,7 +12,6 @@ var allCommands = map[string]int{"echo": 0, "exit": 1, "type": 2, "pwd": 3}
 
 func execCommand(commands []string, pathEnv string) {
 	var resp string
-	//log.Println("The path env is ", pathEnv)
 	switch commands[0] {
 	case "exit":
 		os.Exit(0)
@@ -22,26 +21,6 @@ func execCommand(commands []string, pathEnv string) {
 		fmt.Fprint(os.Stdout, resp)
 
 	case "type":
-		// cmdStr := strings.Join(commands[1:], "")
-
-		// _, exits := allCommands[cmdStr]
-		// if exits {
-		// 	resp = fmt.Sprintf("%s %s\n", cmdStr, "is a shell builtin")
-		// 	fmt.Fprint(os.Stdout, resp)
-		// } else {
-		// 	paths := strings.Split(pathEnv, ":")
-		// 	for _, path := range paths {
-		// 		str := path + "/" + cmdStr
-		// 		_, err := os.Stat(str)
-		// 		if err == nil {
-		// 			resp = fmt.Sprintf("%s is %s\n", cmdStr, str)
-		// 			fmt.Fprint(os.Stdout, resp)
-		// 			return
-		// 		}
-		// 	}
-		// 	resp = fmt.Sprintf("%s: %s\n", cmdStr, "not found")
-		// 	fmt.Fprint(os.Stdout, resp)
-		// }
 
 		typeCommand(commands, pathEnv)
 
