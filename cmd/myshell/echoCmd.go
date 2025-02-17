b1ddc2b3cebefdb52911c6637c73eb87e2cfa264
navneetshukl <navneetshukla824@gmail.com>
2025-02-17 21:09:10 +0530

    some change

--- cmd/myshell/echoCmd.go
+++ cmd/myshell/echoCmd.go
@@ -8,28 +8,32 @@ import (
 
 func echoCmd(commands []string) {
 	//resp := strings.Join(commands[1:], " ")
+	commands = commands[1:]
 	resp := ""
-	//fmt.Println("Commands is ", commands)
 	for _, val := range commands {
-		//fmt.Println("Length and character is ", len(val), val)
-		if len(val) > 0 && val != "echo" {
+		if len(val) > 0 {
 			resp += val + " "
 		}
 	}
-	for idx := range commands {
-		commands[idx] = strings.TrimSpace(commands[idx])
-	}
-	resp = fmt.Sprintf("%s\n", resp)
+	//fmt.Println("Resp is ",resp)
+	//fmt.Println("Length is ",len(resp))
+	// for idx,val:=range resp{
+	// 	fmt.Println("Index and string is ",idx,string(val))
+	// }
+	isquote:=false
 
 	ch1 := resp[0]
 	if string(ch1) == "'" {
 		resp = resp[1:]
+		isquote=true
 	}
+	//fmt.Println("char is ",string(resp[len(resp)-2]))
+	resp=strings.TrimSpace(resp)
 
-	if string(resp[len(resp)-2]) == "'" {
+	if isquote {
 		resp1 := ""
-		for idx, val := range resp {
-			if idx == len(resp)-2 && string(val) == "'" {
+		for _, val := range resp {
+			if string(val) == "'" {
 				continue
 			} else {
 				resp1 += string(val)
@@ -37,6 +41,7 @@ func echoCmd(commands []string) {
 		}
 		resp = resp1
 	}
+	resp+="\n"
 	fmt.Fprint(os.Stdout, resp)
 }
 
